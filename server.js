1a6791e7d4a377e94b9cffe39aa3a9263199e89e
Forrest Van Duren <forrestvanduren1@gmail.com>
2022-07-25 01:25:22 -0600

    ...

--- server.js
+++ server.js
@@ -1390,6 +1390,7 @@ app.get('/forums/([^/]+)/([^/]+)', (req, res) => {
         where ${req.url.substring(8, lastSlash).toLowerCase()}posts.threadid = '${req.url.slice(threadid + 1, req.url.lastIndexOf('_'))}' 
         and ${req.url.substring(8, lastSlash).toLowerCase()}posts.username = users.name
         and ${req.url.substring(8, lastSlash).toLowerCase()}threads.title = '${title}'
+        and ${req.url.substring(8, lastSlash).toLowerCase()}threads.id = '${req.url.slice(threadid + 1, req.url.lastIndexOf('_'))}'
         order by ${req.url.substring(8, lastSlash).toLowerCase()}posts.id asc
         limit 20 offset ${offset - 20}`, (err, resp) => {
             
